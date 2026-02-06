be898936f4cdc75900ad750c4ad48e409bb4a7a5
landon.noss <landon.noss@sony.com>
2026-02-06 00:15:38 -0700

    Remove remaining game test hooks

--- server/test/test_helper.exs
+++ server/test/test_helper.exs
@@ -1,10 +1,2 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Tostada.Repo, :manual)
-
-unless Code.ensure_loaded?(Tostada.World.Broadcasters.TestProbe) do
-  Code.require_file("support/broadcasters/test_probe.ex", __DIR__)
-end
-
-unless Code.ensure_loaded?(Tostada.Tasks.TestOutcomeHook) do
-  Code.require_file("support/hooks/test_outcome_hook.ex", __DIR__)
-end
